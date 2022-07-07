525cd71b3f2b6e613ffdf8347df15b6cb751db21
ozalakshay <lakshayoza126@gmail.com>
2022-07-07 11:59:49 +0530

    Add other features successfully and tested

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,22 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import {
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+  createUser,
+} from "../controllers/users.js";
 
 const router = express.Router();
-let users = [
-];
 
-router.get("/", (req, res) => {
-  // res.send(`<p>Welcome to the user routes</p>`);
-  res.send(users);
-  // console.log("hi from user route");
-});
+router.get("/", getUsers);
 
-router.post("/", (req, res) => {
-  // console.log(req.body);
-  const user = req.body;
-  // const userId = uuidv4();
+router.post("/", createUser);
 
-  // const userWithId = { ...user, id: uuidv4() };
-  users.push({ ...user, id: uuidv4() });
-  // console.log(req.body);
-});
+router.get("/:id", getUser);
 
-// console.log(users, users.length);
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", updateUser);
 
 export default router;
